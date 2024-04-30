0251cc548c8bbcc1192d44a54a177b59aee8ebcd
Dulce Herrera <dulceherrera11@gmail.com>
2024-04-30 09:05:58 -0700

    advances

--- src/controllers/purchase.controller.js
+++ src/controllers/purchase.controller.js
@@ -2,28 +2,39 @@ const catchError = require('../utils/catchError');
 const Purchase = require('../models/Purchase');
 const Product = require('../models/Product')
 const Cart = require('../models/Cart')
+const Category = require("../models/Category")
 
 
 const getAll = catchError(async(req, res) => {
-    const { id } = req.user
+    const userId = req.user.id
     const purchase = await Purchase.findAll({
+      where: { userId },
       include: [{
-        model: Product
-      }],
-      where: {userId: id}
+        model: Product,
+        attributes: {exclude: ["createdAt", "updatedAt"]},
+        include: [{
+          model: Category,
+          attributes: ["name"]
+        }]
+      }]
     });
     return res.json(purchase);
 });
 
 const create = catchError(async(req, res) => {
+  const userId =  req.user.id
     const cart = await Cart.findAll({
-      where: {userId: req.user.id},
+      where: {userId},
       attributes: ['quantity', 'userId', 'productId'],
       raw: true
     })
+
+    if (!cart) res.sendStatus(404)
+
     const purchase = await Purchase.bulkCreate(cart)
-    await Cart.destroy({ where: { userId:req.user.id } })
-    return res.json(purchase)
+    await Cart.destroy({ where: { userId } })
+
+    return res.status(201).json(purchase)
 });
 
 
